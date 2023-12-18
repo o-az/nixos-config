a4372d9c77e42e6e659b0b7d8fbd7b0e4046e711
Omar <github@omaraziz.dev>
2023-12-18 13:15:09 -0800

    feat: initial vm setup

--- flake.nix
+++ flake.nix
@@ -1,5 +1,16 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {};
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      vm-orb = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./modules/configuration.nix
+          ./modules/lxd.nix
+          ./modules/orbstack.nix
+        ];
+      };
+    };
+  };
 }
