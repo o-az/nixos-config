d5bc95dc96ce7d5fcc7f949851602cfce2d48cb0
Omar <github@omaraziz.dev>
2024-01-05 06:56:56 -0800

    Update users.nix

--- modules/users.nix
+++ modules/users.nix
@@ -3,12 +3,12 @@
     mutableUsers = false;
     users.o = {
       isNormalUser = true;
-      home = "/home/o";
+      home = "/home/omar";
       extraGroups = [ "docker" "wheel" ];
       # shell = pkgs.zsh;
       hashedPassword = "$y$j9T$dHqYv21jcB7S3jBrqffWx1$xUqbav37bG7iWc2A1bFUvPrwTB8.wCUXCZdm1QNb5n2";
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGUVfIB/tLqTsawr6wzMxa9TJ0llgFEzSiRyFtDkV/gb"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1YmsLW3HzJZmw2iz3ii0y+zy3IRM0lcKZW04lms2dC"
       ];
     };
   };
