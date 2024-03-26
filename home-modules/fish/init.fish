ec3540dc538efdc72c661693a7be0170bb9c5798
omar <omaraziz.dev@proton.me>
2024-03-25 21:34:10 -0700

    chore: lorem ipsum

--- home-modules/fish/init.fish
+++ home-modules/fish/init.fish
@@ -12,28 +12,6 @@ set -Ux XDG_CONFIG_HOME ~/.config
 
 zoxide init fish | source
 
-# forgit
-set -gx PATH $PATH $FORGIT_INSTALL_DIR/bin
-set -U FORGIT_CHECKOUT_BRANCH_BRANCH_GIT_OPTS '--sort=-committerdate'
-set -gx forgit_log glo
-set -gx forgit_diff gd
-set -gx forgit_add ga
-set -gx forgit_reset_head grh
-set -gx forgit_ignore gi
-set -gx forgit_checkout_file gcf
-set -gx forgit_checkout_branch gcb
-set -gx forgit_branch_delete gbd
-set -gx forgit_checkout_tag gct
-set -gx forgit_checkout_commit gco
-set -gx forgit_revert_commit grc
-set -gx forgit_clean gclean
-set -gx forgit_stash_show gss
-set -gx forgit_stash_push gsp
-set -gx forgit_cherry_pick gcp
-set -gx forgit_rebase grb
-set -gx forgit_blame gbl
-set -gx forgit_fixup gfu
-
 # direnv
 direnv hook fish | source
 set -g direnv_fish_mode eval_on_arrow # trigger direnv at prompt, and on every arrow-based directory change (default)
@@ -51,3 +29,7 @@ set -Ux FZF_DEFAULT_OPTS "--height 100% --inline-info --layout=reverse --margin=
 set fzf_git_log_format "%H %s"
 set fzf_history_time_format "%y-%m-%d"
 set fzf_diff_highlighter diff-so-fancy
+
+# forgit
+set -x PATH $PATH $FORGIT_INSTALL_DIR/bin
+set -Ux FORGIT_CHECKOUT_BRANCH_BRANCH_GIT_OPTS '--sort=-committerdate'
