9bbce11f1420dccec65e1ee44e1223dde5d87873
omar <omaraziz.dev@proton.me>
2024-05-07 12:57:29 -0700

    feat: flakehub cli

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -36,6 +36,7 @@ with lib;
         fx
         jo
         jq
+        fh
         yq
         gh
         niv
--- modules/nix.nix
+++ modules/nix.nix
@@ -22,11 +22,13 @@
       trusted-substituters = [
         "https://nix-community.cachix.org"
         "https://union.cachix.org"
+        "https://cache.flakehub.com"
       ];
       trusted-public-keys = [
         "union.cachix.org-1:TV9o8jexzNVbM1VNBOq9fu8NK+hL6ZhOyOh0quATy+M="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "cache.flakehub.com-1:t6986ugxCA+d/ZF9IeMzJkyqi5mDhvFIx7KA/ipulzE= cache.flakehub.com-2:ntBGiaKSmygJOw2j1hFS7KDlUHQWmZALvSJ9PxMJJYU="
       ];
     };
     extraOptions = ''
