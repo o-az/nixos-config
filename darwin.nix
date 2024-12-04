88774eb6835e0f2bb17b0a222e09e43b7914da99
o-az <omaraziz.dev@proton.me>
2024-12-04 01:34:21 -0800

    chore: sigh

--- darwin.nix
+++ darwin.nix
@@ -30,6 +30,86 @@ nix-darwin.lib.darwinSystem rec {
     ./modules/darwin.nix
     # ./modules/fonts.nix
     ./modules/nix.nix
+    {
+      homebrew = {
+        enable = true;
+        onActivation = {
+          cleanup = true;
+          upgrade = true;
+          autoUpdate = true;
+        };
+        global.autoUpdate = true;
+        masApps = {
+          "Bitwarden" = 1352778147;
+        };
+        taps = [
+          "homebrew/services"
+        ];
+        casks = [
+          "iina"
+          "warp"
+          "kitty"
+          "qview"
+          "stats"
+          "gather"
+          "notion"
+          "signal"
+          "alt-tab"
+          "anytype"
+          "discord"
+          "element"
+          "raycast"
+          "simplex"
+          "vivaldi"
+          "xquartz"
+          "obsidian"
+          "orbstack"
+          "qflipper"
+          "telegram"
+          "transmit"
+          "1password"
+          "cleanshot"
+          "hiddenbar"
+          "parallels"
+          "pixelsnap"
+          "protonvpn"
+          "appcleaner"
+          "charmstone"
+          "cheatsheet"
+          "knockknock"
+          "mullvadvpn"
+          "pdf-expert"
+          "proton-mail"
+          "proton-pass"
+          "zed@preview"
+          "micro-snitch"
+          "transmission"
+          "betterdisplay"
+          "brave-browser"
+          "little-snitch"
+          "screen-studio"
+          "beekeeper-studio"
+          "eloston-chromium"
+          "karabiner-elements"
+          "sony-ps-remote-play"
+          "firefox@developer-edition"
+          "visual-studio-code@insiders"
+
+          # TODO: figure out font sharing
+          #
+          # "font-monaspace"
+          # "font-ibm-plex-mono"
+          # "font-3270-nerd-font"
+          # "font-hack-nerd-font"
+          # "font-jetbrains-mono"
+          # "font-0xproto-nerd-font"
+          # "font-blex-mono-nerd-font"
+          # "font-fira-code-nerd-font"
+          # "font-monaspace-nerd-font"
+          # "font-jetbrains-mono-nerd-font"
+        ];
+      };
+    }
     #
     # home-manager
     #
@@ -130,79 +210,4 @@ nix-darwin.lib.darwinSystem rec {
     #   };
     # }
   ];
-  # homebrew = {
-  #   enable = true;
-  #   onActivation = {
-  #     cleanup = true;
-  #     upgrade = true;
-  #     autoUpdate = true;
-  #   };
-  #   global.autoUpdate = true;
-  #   masApps = {
-  #     "Bitwarden" = 1352778147;
-  #   };
-  #   casks = [
-  #     "iina"
-  #     "warp"
-  #     "kitty"
-  #     "qview"
-  #     "stats"
-  #     "gather"
-  #     "notion"
-  #     "signal"
-  #     "alt-tab"
-  #     "anytype"
-  #     "discord"
-  #     "element"
-  #     "raycast"
-  #     "simplex"
-  #     "vivaldi"
-  #     "xquartz"
-  #     "obsidian"
-  #     "orbstack"
-  #     "qflipper"
-  #     "telegram"
-  #     "transmit"
-  #     "1password"
-  #     "cleanshot"
-  #     "hiddenbar"
-  #     "parallels"
-  #     "pixelsnap"
-  #     "protonvpn"
-  #     "appcleaner"
-  #     "charmstone"
-  #     "cheatsheet"
-  #     "knockknock"
-  #     "mullvadvpn"
-  #     "pdf-expert"
-  #     "proton-mail"
-  #     "proton-pass"
-  #     "zed@preview"
-  #     "micro-snitch"
-  #     "transmission"
-  #     "betterdisplay"
-  #     "brave-browser"
-  #     "little-snitch"
-  #     "screen-studio"
-  #     "beekeeper-studio"
-  #     "eloston-chromium"
-  #     "karabiner-elements"
-  #     "sony-ps-remote-play"
-  #     "firefox@developer-edition"
-  #     "visual-studio-code@insiders"
-
-  #     # TODO: figure out font sharing
-  #     #
-  #     # "font-monaspace"
-  #     # "font-ibm-plex-mono"
-  #     # "font-3270-nerd-font"
-  #     # "font-hack-nerd-font"
-  #     # "font-jetbrains-mono"
-  #     # "font-0xproto-nerd-font"
-  #     # "font-blex-mono-nerd-font"
-  #     # "font-fira-code-nerd-font"
-  #     # "font-monaspace-nerd-font"
-  #     # "font-jetbrains-mono-nerd-font"
-  #   ];
-  # };
 }
--- modules/darwin.nix
+++ modules/darwin.nix
@@ -11,7 +11,6 @@
     name = "o";
     home = "/Users/o";
   };
-  services.nix-daemon.enable = true;
   environment = {
     systemPackages = with pkgs; [
       gh
@@ -40,7 +39,29 @@
       localHostName = name;
     };
 
+  nix.settings.trusted-users = [ user ];
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 4;
+  system = {
+    stateVersion = 5;
+    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+    activationScripts.postUserActivation.text = ''
+      # activateSettings -u will reload the settings from the database and apply them to the current session,
+      # so we do not need to logout and login again to make the changes take effect.
+      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
+
+    defaults = {
+      menuExtraClock.Show24Hour = true; # show 24 hour clock
+
+      # other macOS's defaults configuration.
+      # ......
+    };
+  };
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.enableSudoTouchIdAuth = true;
+
+  programs.zsh.enable = true;
 }
